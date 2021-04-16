62b6b4310702441875253d1b63e33005a2597414
이상훈 <AE201214@etoos.com>
2021-04-16 14:52:44 +0900

    config 제거 후 repository 재생성

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # make_daily
-Selenium, 공공데이터 Open API를 이용한 데일리 회의록 자동생성
+selenium, 공공데이터 Open API를이용한 회의록 자동생성 
+
+주의사항 : chormedriver와 chrome 버전을 맞춰줘야함
+1. chrome://version 버전 확인
+2. https://sites.google.com/a/chromium.org/chromedriver/downloads 같은 버전 다운르드
---
+++ make_daily.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+# 주의사항 : chormedriver와 chrome 버전을 맞춰줘야함. 
+#
+import config
+import requests
+import time
+from selenium import webdriver
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+etoos_id = config.etoos_id
+etoos_pw = config.etoos_pw
+serviceKey = config.serviceKey
+
+today = datetime.today()
+
+# 공공데이터 OPEN API query 생성(특일정보)
+def get_request_query(url, operation, params, serviceKey):
+    import urllib.parse as urlparse
+    params = urlparse.urlencode(params)
+    request_query = url + '/' + operation + '?' + params + '&' + 'serviceKey' + '=' + serviceKey
+    return request_query
+    
+#휴일 확인
+def check_restday(findday, count):
+    count=count+1
+    findday = findday - timedelta(1)
+    year = findday.year
+    month = findday.month
+    day = findday.day
+    if month < 10:
+        month = '0' + str(month)
+    else:
+        month = str(month)
+
+    #공휴일 확인
+    url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService'#공공데이터 OPEN API(특일정보) URL
+    operation = 'getRestDeInfo'
+    params = {'solYear':year, 'solMonth':month}
+    request_query = get_request_query(url, operation, params, serviceKey)
+    get_data = requests.get(request_query)    
+    if True == get_data.ok:
+        soup = BeautifulSoup(get_data.content, 'html.parser')        
+        
+        item = soup.findAll('item')
+        #print(item);
+        for i in item:
+            if int(i.locdate.string[4:6]) == int(day):
+                return check_restday(findday,count)
+    #주말 확인
+    weekday = findday.weekday()
+    if (weekday == 5 or weekday == 6) :
+        return check_restday(findday,count)
+    
+    #휴일 수 리턴
+    return findday
+
+#이전 데일리 날짜
+find_day = (check_restday(today,0)).strftime("%Y-%m-%d")
+#금일 데일리 날짜
+today =today.strftime('%Y-%m-%d')
+
+# chorme driver
+driver = webdriver.Chrome('./chromedriver')
+
+# 데일리생성 - A
+url = 'https://doc.etoos.com/display/ONLINE/'+find_day+'+DAILY+A'
+driver.get(url)
+
+driver.find_element_by_name('os_username').send_keys(etoos_id)
+driver.find_element_by_name('os_password').send_keys(etoos_pw)
+driver.find_element_by_id('loginButton').click()
+
+time.sleep(1)
+
+menu = driver.find_element_by_xpath ('//*[@id="action-menu-link"]')
+menu.click()
+
+time.sleep(1)
+
+copy = driver.find_element_by_xpath ('//*[@id="action-copy-page-link"]')
+copy.click()
+
+driver.implicitly_wait(30)
+time.sleep(1)
+
+copy_next = driver.find_element_by_xpath ('//*[@id="copy-dialog-next"]')
+copy_next.click()
+
+driver.implicitly_wait(30)
+time.sleep(1)
+
+title = today + " DAILY A"
+driver.find_element_by_css_selector("input#content-title.text.pagetitle").send_keys(title)
+
+time.sleep(1)
+driver.implicitly_wait(100)
+
+make = driver.find_element_by_xpath ('//*[@id="rte-button-publish"]')
+make.click()
+
+
+# 데일리생성 - B
+url = 'https://doc.etoos.com/display/ONLINE/'+find_day+'+DAILY+B'
+driver.get(url)
+
+time.sleep(1)
+
+menu = driver.find_element_by_xpath ('//*[@id="action-menu-link"]')
+menu.click()
+
+time.sleep(1)
+
+copy = driver.find_element_by_xpath ('//*[@id="action-copy-page-link"]')
+copy.click()
+
+driver.implicitly_wait(30)
+time.sleep(1)
+
+copy_next = driver.find_element_by_xpath ('//*[@id="copy-dialog-next"]')
+copy_next.click()
+
+driver.implicitly_wait(30)
+time.sleep(1)
+
+title = today + " DAILY B"
+driver.find_element_by_css_selector("input#content-title.text.pagetitle").send_keys(title)
+
+time.sleep(1)
+driver.implicitly_wait(100)
+
+make = driver.find_element_by_xpath ('//*[@id="rte-button-publish"]')
+make.click()
+
+time.sleep(1)
+driver.implicitly_wait(100)
+
+driver.quit()
+
